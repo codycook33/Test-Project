153d28c59ca56420d0a1a27233008df8aac35447
Cody Cook <codycook33@gmail.com>
2018-09-06 19:23:44 -0700

    Test stuff, player movement, small level

---
+++ Assets/BuildCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildCube : MonoBehaviour {
+
+	public MeshFilter filter;
+	public Vector3[] verts;
+	public int[] tris;
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+			
+		Mesh myNewMesh = new Mesh();
+		myNewMesh.vertices = verts;
+		myNewMesh.triangles = tris;
+
+		filter.mesh = myNewMesh;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float speed;
+	public float jumpPower;
+	// Use this for initialization
+	void Start () {
+		speed = 8;
+		jumpPower = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Jump") * jumpPower * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	}
+}
