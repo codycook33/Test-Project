8f9b1020476d4f8f05e3a3e1a2041bdc709028d5
Cody Cook <codycook33@gmail.com>
2018-09-07 23:55:45 -0700

    working on velocity movement. Added velocity to jump

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,14 +5,46 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 	public float speed;
 	public float jumpPower;
+	public Rigidbody rb;
+	private bool isGrounded;
+
+	
+	
 	// Use this for initialization
 	void Start () {
 		speed = 8;
 		jumpPower = 10;
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Jump") * jumpPower * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	void FixedUpdate()
+	{
+		//transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Jump") * jumpPower * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+		if (isGrounded)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				rb.velocity = new Vector3(0f, jumpPower, 0f);
+			}
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
+		
+	}
+	void OnCollisionExit(Collision other)
+	{
+		if (other.gameObject.tag == "Ground")
+		{
+			isGrounded = false;
+		}
+		
 	}
 }
